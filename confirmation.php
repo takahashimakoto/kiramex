07019b8f78c0872208b80af777f1a5b6788aa386
takahashimakoto <takahashi.c.colors@gmail.com>
2015-06-23 13:24:46 +0900

    いけすかない

--- confirmation.php
+++ confirmation.php
@@ -26,7 +26,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 //// webpay end
 
 //// mailer start
-$mail = new PHPMailer;
 
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 $mail->isSMTP();                                      // Set mailer to use SMTP
@@ -34,19 +33,19 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 //$mail->Username = 'user@example.com';                 // SMTP username
 //$mail->Password = 'secret';                           // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+//$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 //$mail->Port = 587;                                    // TCP port to connect to
 
-$mail->From = 'from@example.com';
-$mail->FromName = 'Mailer';
-$mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-$mail->addAddress('ellen@example.com');               // Name is optional
-$mail->addReplyTo('info@example.com', 'Information');
-$mail->addCC('cc@example.com');
-$mail->addBCC('bcc@example.com');
+$mail->From = 'musubi151515@gmail.com';
+$mail->FromName = 'from-musubi';
+$mail->addAddress('musubi151515@gmail.com', 'Joe User');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+$mail->addReplyTo('musubi151515@gmail.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
 
-$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
 
 $mail->Subject = 'Here is the subject';
