3e5543028e9dfac09bf7a08d5e338bd1298a48f8
sigel rotten <rottensigel@gmail.com>
2015-06-23 17:25:15 +0900

    カートに表示！

--- cart.php
+++ cart.php
@@ -14,13 +14,82 @@
 	<br>
 
 	<?php
+session_start();
+$connect = mysql_connect("localhost","root","");
+$db = "musubi";
+//SQLをUTF8形式で書くよ、という意味
+mysql_query("SET NAMES utf8",$connect);
 
-	session_start();
+//ここでおにぎりのデータベース情報をデータベースから取る
+$i = 1;
+$result1=mysql_db_query("musubi","SELECT * from items");
 
-	echo "<pre>";
-	print_r($_SESSION["order"]);
-	echo "</pre>";
+while(true){
+      $kekka1 = mysql_fetch_assoc($result1);
+      if($kekka1 == null) {
+        break;
+      }else{
+        $i++;
+           // echo"<br>";
+            $items[$kekka1['item_id']] = $kekka1;
+          //  echo"<br>";
+ 
+       }
+} 
+   print_r($items);
+//ここからお米のデータベース情報をデータベースから取る
+$i = 1;
+$result2=mysql_db_query("musubi","SELECT * from rices");
 
+while(true){
+      $kekka2 = mysql_fetch_assoc($result2);
+      if($kekka2 == null) {
+        break;
+      }else{
+        $i++;
+           // echo"<br>";
+            $rices[$kekka2['rice_id']] = $kekka2;
+          //  echo"<br>";
+    
+    }
+}
+
+//ここからのりのデータベース情報をデータベースから取る
+$i = 1;
+$result3=mysql_db_query("musubi","SELECT * from noris");
+
+while(true){
+      $kekka3 = mysql_fetch_assoc($result3);
+      if($kekka3 == null) {
+        break;
+      }else{
+        $i++;
+           // echo"<br>";
+            $noris[$kekka3['nori_id']] = $kekka3;
+          //  echo"<br>";
+    
+    }
+}
+
+
+
+			echo "<p>カートの中身<p><br>";
+			echo "<pre>";
+			$i = 0;
+			foreach ($_SESSION["order"] as $orders) {
+			/*	echo $orders['具']; */
+				$i++;
+				echo "注文".$i."<br>" ;
+				$gu =  $orders['具'];
+				echo $items[$gu]['item_name']."<br>";
+				$kome =  $orders['米'];
+				echo $rices[$kome]['rice_name']."<br>";
+				$nori =  $orders['海苔'];
+				echo $noris[$nori]['nori_name']."<br>";
+
+
+			}
+			echo "</pre>";
 	?>
 </body>
 
