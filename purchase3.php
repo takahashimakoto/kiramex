a92a47f51d6557a0007527746c3ae707dcb579c9
sigel rotten <rottensigel@gmail.com>
2015-06-25 10:42:27 +0900

    sqlファイルだよ

---
+++ purchase3.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+	<meta content="text/html" charset="UTF-8">
+</head>
+
+<body>
+<form action = "confirmation.php" method = "POST">
+<p>名前：</p>p<input type = "text" name = "name" value = "" placeholder = "名前">
+<p>住所：</p><input type = "text" name = "address" value = "" placeholder = "住所">
+<a href = "http://localhost/musubi/cart.php">カートに戻る</a>
+<a href = "http://localhost/musubi/confirmation.php">購入画面へ進む</a>
+
+
+
+
+</form>
+</body>
+
+</html>
